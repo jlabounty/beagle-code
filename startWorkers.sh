a4a644ab312b8bb28e171834e0c445a291154faf
UW Muon Lab <uw.muon.dropbox@gmail.com>
2016-10-12 13:28:08 -0500

    fixed startworkers so it works for all ip addresses

--- startWorkers.sh
+++ startWorkers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pushd /home/g-2/beagle-code
-masteripstr=`ifconfig eth0 | grep 192.168.1.21`
+masteripstr=`ifconfig eth0 | grep '192.168.\{2,4\}.21'`
 [[ ! -z $masteripstr ]] && ./startMasterWorkers.sh || ./startSlaveWorkers.sh
 popd
 exit 0
